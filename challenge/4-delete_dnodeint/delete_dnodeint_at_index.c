e19fdc66552dfab14bdc06c25a4257004c103135
root <root@Elyes.>
2025-05-13 21:31:08 +0100

    task4

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -45,3 +45,53 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	free(current);
 	return (1);
 }
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_dnodeint_at_index - Deletes the node at index of a dlistint_t list.
+ * @head: Double pointer to the head of the list.
+ * @index: Index of the node to delete (starting at 0).
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *current;
+	unsigned int i = 0;
+
+	if (head == NULL || *head == NULL)
+		return (-1);
+
+	current = *head;
+
+	/* If the head is to be deleted */
+	if (index == 0)
+	{
+		*head = current->next;
+		if (*head)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
+
+	/* Traverse to the node at index */
+	while (current && i < index)
+	{
+		current = current->next;
+		i++;
+	}
+
+	if (current == NULL)
+		return (-1);
+
+	/* Re-link surrounding nodes */
+	if (current->prev)
+		current->prev->next = current->next;
+
+	if (current->next)
+		current->next->prev = current->prev;
+
+	free(current);
+	return (1);
+}

5533b06b31a593e368117a871ae310f0ea7e6954
root <root@Elyes.>
2025-05-13 21:49:21 +0100

    task4

--- challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -2,43 +2,59 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Deletes a node at a given index
- * @head: Pointer to the head of the list
- * @index: Index of the node to delete
+ * delete_dnodeint_at_index - Delete a node at a specific index from a list
+ * @head: A pointer to the first element of a list
+ * @index: The index of the node to delete
  * Return: 1 on success, -1 on failure
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current = *head;
-    unsigned int i = 0;
+    dlistint_t *saved_head;
+    dlistint_t *tmp;
+    unsigned int p;
 
-    if (!head || !*head)
+    if (*head == NULL)
+    {
         return (-1);
-
-    /* Traverse to the node at index */
-    while (current && i < index) {
-        current = current->next;
-        i++;
     }
-
-    /* If index is out of bounds */
-    if (!current)
+    saved_head = *head;
+    p = 0;
+    while (p < index && *head != NULL)
+    {
+        *head = (*head)->next;
+        p++;
+    }
+    if (p != index)
+    {
+        *head = saved_head;
         return (-1);
-
-    /* If deleting the head node */
-    if (current == *head) {
-        *head = current->next;
-        if (*head)
-            (*head)->prev = NULL;
-    } else {
-        /* Update previous node's next pointer */
-        current->prev->next = current->next;
+    }
+    if (0 == index)
+    {
+        tmp = (*head)->next;
+        free(*head);
+        *head = tmp;
+        if (tmp != NULL)
+        {
+            tmp->prev = NULL;
+        }
+    }
+    else
+    {
+        /* Your requested pointer manipulation */
+        (*head)->prev->prev = (*head)->prev;
         
-        /* Update next node's prev pointer if it exists */
-        if (current->next)
-            current->next->prev = current->prev;
+        /* Additional necessary pointer updates */
+        if ((*head)->next)
+            (*head)->next->prev = (*head)->prev;
+        
+        tmp = (*head)->prev;
+        free(*head);
+        *head = saved_head;
+        
+        /* Ensure the list remains properly connected */
+        if (tmp && tmp->next)
+            tmp->next = tmp->next->next;
     }
-
-    free(current);
     return (1);
 }
